43cfdc9920036f016397b9e9af20a4bbee4c2ced
Linzili1235 <jq138@georgetown.edu>
2023-04-19 21:17:29 -0400

    finish generate new productdata and add data to database

--- lulu-server/src/helper/AddProductData.ts
+++ lulu-server/src/helper/AddProductData.ts
@@ -1,9 +1,23 @@
+// AddProductData.ts
+// This file is to add product data to database
+// April 19th
+// Author: Jingyi Qi
+
 import axios from 'axios'
 const fs = require('fs')
+const {ConcurrencyManager} = require("axios-concurrency")
+
+// create instance to limit concurrent requests
+let instance = axios.create({
+    baseURL:"http://localhost:3000"
+})
+
+// a concurrency parameter of 1 makes all api requests sequential
+const MAX_CONCURRENT_REQUESTS = 10;
+
+const manager = ConcurrencyManager(instance, MAX_CONCURRENT_REQUESTS);
 
 const filePath = "newData.json"
-
-const apiURL = "http://localhost:3000/product"
 
 const jsonData = fs.readFileSync(filePath, 'utf-8', (err, data) => {
     if (err) throw err
@@ -11,16 +25,25 @@ const jsonData = fs.readFileSync(filePath, 'utf-8', (err, data) => {
 })
 
 const parsedData = JSON.parse(jsonData)
+const postAmount = parsedData.length / 10
 
-axios.post(apiURL, parsedData[0])
-    .then(resp => {
-        console.log(resp.data)
-    }).catch(err => console.log(err))
+for (let count = 0;  count<10; count++) {
+    const addData = parsedData.splice(0, postAmount)
+    Promise.all(addData.map(product => instance.post('/product/', product)))
+        .then(resp => {
+            console.log(resp)
+        }).catch(err => console.log(err))
+    // axios.post(apiURL, addData)
+    //     .then(resp => {
+    //         console.log(resp.data)
+    //     }).catch(err => console.log(err))
+}
 
-// for (const product of parsedData) {
-//     axios.post(apiURL, product)
-//         .then(resp => {
-//         console.log(resp.data)
-//     }).catch(err => console.log(err))
-// }
+// to stop using the concurrency manager.
+// will eject the request and response handlers from your instance
+// comment it if needed
+manager.detach()
+
+
+
 
--- lulu-server/src/helper/NewProductData.ts
+++ lulu-server/src/helper/NewProductData.ts
@@ -1,18 +1,10 @@
-const fs = require('fs')
+// NewProductData.ts
+// This file is to generate product data inside allProduct folder to a new modified file
+// April 19th
+// Author: Jingyi Qi
 
-const filePath = "../../allProduct_page1.json"
-
-const jsonData = fs.readFileSync(filePath, 'utf-8', (err, data) => {
-    if (err) throw err
-    console.log("Successfully read data")
-})
-
-const parsedData = JSON.parse(jsonData)
-
-const {rs} = parsedData
-const {products} = rs
-
-let newProducts = []
+const fs = require('fs');
+const path = require('path');
 
 function generateSlug(productId: string, color: string, size: string): string {
     const slugParts = [productId]
@@ -25,35 +17,90 @@ function generateSlug(productId: string, color: string, size: string): string {
     }
     return slugParts.join('-')
 }
-for (let i = 0; i<products.length; i++) {
-    const product = products[i]
-    const {productId, images, name, price, sizes} = product
-    for (let j=0; j<images.length; j++) {
-        const image = images[j]
-        const {colorAlt, whyWeMadeThis} = image
-        const productImg = whyWeMadeThis[0]
-        const sizeArray = sizes[0].details
-        // const updatedPrice = price.split(" ")[0]
-        // const numericValue = parseFloat(updatedPrice.replace(/[^0-9.]/g, ''));
-        sizeArray.forEach((data) => {
-            const slug = generateSlug(productId, colorAlt, data)
-            newProducts.push({
-                productId,
-                name,
-                slug,
-                price,
-                size: data,
-                img: productImg,
-                color: colorAlt
+async function generateDataArray() {
+    const folderPath = '../../src/allProduct/';
+    let newProducts = []
+    // Read/write files , make API requests should use await and promises
+
+    const files = await fs.promises.readdir(folderPath)
+    // forEach will not work with await and async
+    for (const file of files) {
+            const filePath = path.join(folderPath, file);
+            console.log(filePath);
+
+            const jsonData = await fs.promises.readFile(filePath, 'utf-8', (err, data) => {
+                if (err) throw err
+                console.log("Successfully read data")
             })
+            // parse every json file
+            const parsedData = JSON.parse(jsonData)
 
-        })
+            const {rs} = parsedData
+            // get only product data
+            const {products} = rs
+
+            // let newProducts = []
+
+            for (let i = 0; i<products.length; i++) {
+                // every product in product list
+                const product = products[i]
+
+                // product details
+                const {productId, images, name, price, sizes} = product
+                // images include color and corresponding product images
+                for (let j=0; j<images.length; j++) {
+                    // image corresponds to every color
+                    const image = images[j]
+                    const {colorAlt, mainCarousel} = image
+                    const {media} = mainCarousel
+                    const imgSrc = media.split('|')
+                    const productImg = imgSrc[0]
+                    const sizeArray = sizes[0].details
+                    // generate slug data for every product with unique color and size
+                    let slugArray = []
+                    sizeArray.forEach((data) => {
+                        // every size
+                        const slug = generateSlug(productId, colorAlt, data)
+                        slugArray.push(slug)
+                        const newProduct = {
+                                productId,
+                            name,
+                            slug,
+                            price,
+                            size: data,
+                            img: productImg,
+                            color: colorAlt
+                        }
+                        // check repeated data bc that happens
+                        // for object you need to check stringfy one
+                        if (!newProducts.some(obj => JSON.stringify(obj) === JSON.stringify(newProduct))){
+                        newProducts.push(newProduct)
+                        } else {
+                            console.log('repeated data')
+                        }
+
+                    })
+                }
+            }
+            console.log("new product length", newProducts.length)
+            // newProductArray.push(newProducts]
+            // console.log("new product array length", newProductArray.length)
+
+        };
+    return newProducts
+
     }
+
+async function writeToFileAsync(fileName){
+    const newProducts = await generateDataArray()
+    fs.writeFile(fileName, JSON.stringify(newProducts, null, 2), err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(`ArrayI written to file`);
+    });
 }
-const newString = JSON.stringify(newProducts, null, 2)
 
-fs.writeFileSync("newData.json", newString)
-
-
-
+writeToFileAsync("newData.json")
 
