26f8315b7eb24d3e9fd71df85cd4181e821a37ef
Linzili1235 <jq138@georgetown.edu>
2023-05-17 00:28:42 -0400

    add one size product to newData js

--- lulu-server/src/helper/NewProductData.ts
+++ lulu-server/src/helper/NewProductData.ts
@@ -48,6 +48,7 @@ async function generateDataArray() {
 
                 // product details
                 const {productId, images, name, price, sizes} = product
+
                 // images include color and corresponding product images
                 for (let j=0; j<images.length; j++) {
                     // image corresponds to every color
@@ -59,8 +60,26 @@ async function generateDataArray() {
                     const sizeArray = sizes[0].details
                     // generate slug data for every product with unique color and size
                     let slugArray = []
-                    sizeArray.forEach((data: string) => {
-                        // every size
+                    // no size data
+                    if (sizeArray.length === 0) {
+                        const slug = generateSlug(productId, colorAlt, "")
+                        slugArray.push(slug)
+                        const newProduct = {
+                            productId,
+                            name,
+                            slug,
+                            price,
+                            size: "One Size",
+                            img: productImg,
+                            color: colorAlt
+                        }
+                        // check repeated data bc that happens
+                        // for object you need to check stringfy one
+                        if (!newProducts.some(obj => JSON.stringify(obj) === JSON.stringify(newProduct))){
+                            newProducts.push(newProduct)
+                        }
+                    }
+                    for (const data of sizeArray) {
                         const slug = generateSlug(productId, colorAlt, data)
                         slugArray.push(slug)
                         const newProduct = {
@@ -72,22 +91,27 @@ async function generateDataArray() {
                             img: productImg,
                             color: colorAlt
                         }
-                        // check repeated data bc that happens
-                        // for object you need to check stringfy one
                         if (!newProducts.some(obj => JSON.stringify(obj) === JSON.stringify(newProduct))){
                         newProducts.push(newProduct)
-                        } else {
-                            console.log('repeated data')
                         }
 
-                    })
+
+
+
+                    }
+
+                // )
                 }
-            }
-            console.log("new product length", newProducts.length)
+
+                }
+        console.log("new product length", newProducts.length)
+
+
+    }
             // newProductArray.push(newProducts]
             // console.log("new product array length", newProductArray.length)
 
-        };
+        ;
     return newProducts
 
     }
